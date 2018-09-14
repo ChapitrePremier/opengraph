bdad6e0b9138fbd5adb85d3b1657555ac2dd2901
Fabien Givors <fabien@emploi-maintenanceindustrielle.com>
2018-09-14 11:13:16 +0200

    Use User-Agent to perform http queries
    - Some website only provide useful meta-tags for some User-Agents

--- opengraph/opengraph.py
+++ opengraph/opengraph.py
@@ -52,7 +52,8 @@ class OpenGraph(dict):
     def fetch(self, url):
         """
         """
-        raw = urllib2.urlopen(url)
+        request = urllib2.Request(url, data=None, headers={ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' })
+        raw = urllib2.urlopen(request)
         html = raw.read()
         return self.parser(html)
 
